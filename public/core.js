79edf613f1a6099c79eb8cc265a3969b4624e294
Warren Wong <wrrnwng@gmail.com>
2015-08-04 11:00:28 -0700

    added get request for etd for selected stations

--- public/core.js
+++ public/core.js
@@ -11,7 +11,7 @@ app.controller('mainController', function($scope, $http) {
   $http.get('/api/trips')
     .success(function(data) {
       $scope.trips = data;
-      console.log(data);
+      // console.log(data);
     })
     .error(function(data) {
       console.log('Error:', data);
@@ -22,7 +22,7 @@ app.controller('mainController', function($scope, $http) {
       .success(function(data) {
         $scope.formData = {};
         $scope.trips = data;
-        console.log(data);
+        // console.log(data);
       })
       .error(function(data) {
         console.log('Error:', data);
@@ -33,25 +33,45 @@ app.controller('mainController', function($scope, $http) {
     $http.delete('/api/trips/' + id)
       .success(function(data) {
         $scope.trips = data;
-        console.log(data);
+        // console.log(data);
       })
       .error(function(data) {
         console.log('Error:', data);
       });
   };
   $scope.getStations = function() {
-    $scope.stations = [];
     $http.get('http://api.bart.gov/api/stn.aspx?cmd=stns&key=MW9S-E7SL-26DU-VV8V')
       .success(function(data) {
         var stations = $($.parseXML(data)).find('stations')[0].children;
         for (var i = 0; i < stations.length; i++) {
-          $scope.stations.push({name: $($(stations[i]).find('name')).text()});
+          $scope.stations = $scope.stations || [];
+          $scope.stations.push({name: $($(stations[i]).find('name')).text(),
+                                abbr: $($(stations[i]).find('abbr')).text()});
         }
-        console.log($scope.stations);
+        // console.log($scope.stations);
       })
       .error(function(data) {
         console.log('Error:', data);
       });  
   };
   $scope.getStations();
+
+  $scope.getStationInfo = function(abbr) {
+    console.log(abbr);
+    $http.get('http://api.bart.gov/api/etd.aspx?cmd=etd&orig=' + abbr + '&key=MW9S-E7SL-26DU-VV8V')
+      .success(function(data) {
+        var estTimes = $($.parseXML(data)).find('etd');
+        for (var i = 0; i < estTimes.length; i++) {
+          console.log(estTimes[i]);
+          $scope.estTimes = $scope.estTimes || [];
+          $scope.estTimes.push({destination: $($(estTimes[i]).find('destination')).text()});
+        }
+        console.log($scope.estTimes);
+        // console.log(data);
+      })
+      .error(function(data) {
+        console.log('Error:', data);
+      });  
+  };
+
 });
