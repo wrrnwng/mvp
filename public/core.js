7764abb21750422dcf9f35db9f245dcf73e4a84b
Warren Wong <wrrnwng@gmail.com>
2015-08-04 17:08:39 -0700

    added routes, but not functional

--- public/core.js
+++ public/core.js
@@ -62,10 +62,10 @@ app.controller('mainController', function($scope, $http) {
 
     $http.get('http://api.bart.gov/api/stn.aspx?cmd=stninfo&orig=' + abbr + '&key=MW9S-E7SL-26DU-VV8V')
       .success(function(data) {
-        var intro = $($.parseXML(data)).find('intro');
-        var food = $($.parseXML(data)).find('food');
-        var shopping = $($.parseXML(data)).find('shopping');
-        var attractions = $($.parseXML(data)).find('attractions');
+        var intro = $($($.parseXML(data)).find('intro')).text();
+        var food = $($($.parseXML(data)).find('food')).text();
+        var shopping = $($($.parseXML(data)).find('shopping')).text();
+        var attractions = $($($.parseXML(data)).find('attractions')).text();
         $scope.stationInfo = {
           intro: intro,
           food: food,
@@ -82,12 +82,15 @@ app.controller('mainController', function($scope, $http) {
         var estTimes = $($.parseXML(data)).find('etd');
         for (var i = 0; i < estTimes.length; i++) {
 
-          $scope.estTimes.push({destination: $($(estTimes[i]).find('destination')).text(),
-                                minutes: $($($(estTimes[i]).find('minutes')).first()).text(),
-                                color: $($($(estTimes[i]).find('color')).first()).text()});
+          $scope.estTimes.push({
+            destination: $($(estTimes[i]).find('destination')).text(),
+            minutes: $($($(estTimes[i]).find('minutes')).first()).text(),
+            color: $($($(estTimes[i]).find('color')).first()).text(),
+            abbr: $($($(estTimes[i]).find('abbreviation')).first()).text()
+          });
         }
         // console.log($scope.estTimes);
-        // console.log(data);
+        console.log(data);
       })
       .error(function(data) {
         console.log('Error:', data);
